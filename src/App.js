66c41257e56a4c4583303b59f7d1e5c7d8ca9c43
Rafael Assad <rafaassadeng@gmail.com>
2020-07-22 14:23:08 -0300

    e lavamos noz

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { MovieDetails, MovieList, EditMovie, NewMovie, NotFound } from './pages/index';
+
 
 function App() {
   return (
-      <div>Movie Card Library CRUD</div>
+    <main>
+      <Switch>
+        <Route exact path='/' component={MovieList} />
+        <Route path='/movies/:id' component={MovieDetails} />
+        <Route path='/movies/new' component={NewMovie} />
+        <Route path='/movies/:id/edit' component={EditMovie} />
+      </Switch>
+    </main>
   );
 }
 
