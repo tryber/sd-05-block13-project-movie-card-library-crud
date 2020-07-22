186558eb7ff731cf27c6aff09713b006efcb2cdb
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-07-22 17:39:54 -0300

    link to movie details set in App and MovieCard according to id

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
 
 function App() {
   return (
   <BrowserRouter>
   <div>Movie Card Library CRUD</div>
   <MovieList />
+  <Switch>
+    <Route to="/movies/:id" component={MovieDetails} />  
+  </Switch>
   </BrowserRouter>
   );
 }
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 
 class MovieCard extends React.Component {
   render() {
+    const { title, storyline, id } = this.props.movie;
     return (
       <div data-testid="movie-card">
         Movie Card
+        <div>{title}</div>
+        <div>{storyline}</div>
+        <Link to={`/movies/${id}`}>VER DETALHES</Link>
       </div>
     );
   }
