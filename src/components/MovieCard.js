3ed30d1dfbe5c79bac72352d95d04531b353cfac
rfreitasbatista <rfreitasbatista@gmail.com>
2020-07-22 16:20:02 -0300

    Requisito 1 e 2 feitos

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -2,9 +2,15 @@ import React from 'react';
 
 class MovieCard extends React.Component {
   render() {
+    const { title, imagePath, subtitle, storyline, genre, rating } = this.props.movie
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <h1>{title}</h1>
+        <img alt="Movie Cover" src={`../${imagePath}`} />
+        <p>{subtitle}</p>
+        <p>{storyline}</p>
+        <p>{genre}</p>
+        <p>{rating}</p>
       </div>
     );
   }
