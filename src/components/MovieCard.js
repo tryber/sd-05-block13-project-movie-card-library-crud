d2bf9ba6609cec269b7037a50a3f569cffaac393
Lucas Costa Ventura <lucascv333@gmail.com>
2020-07-22 16:51:53 -0300

    code climate

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -3,12 +3,14 @@ import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie: { title, imagePath, storyline, id }, } = this.props;
+
     return (
       <div data-testid="movie-card">
-        <h2>{this.props.movie.title}</h2>
-        <img src={this.props.movie.imagePath} alt={`Capa do filme ${this.props.movie.title}`} />
-        <span>{this.props.movie.storyline}</span>
-        <Link to={`movies/:${this.props.movie.id}`}> VER DETALHES </Link>
+        <h2>{title}</h2>
+        <img src={imagePath} alt={`Capa do filme ${title}`} />
+        <span>{storyline}</span>
+        <Link to={`movies/:${id}`}> VER DETALHES </Link>
       </div>
     );
   }
