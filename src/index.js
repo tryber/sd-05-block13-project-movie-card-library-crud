d83fc76f6b7830588180ee0c41c0293874836b23
Lucas Costa Ventura <lucascv333@gmail.com>
2020-07-22 16:42:25 -0300

    code climate

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+  <App />
   </BrowserRouter>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
