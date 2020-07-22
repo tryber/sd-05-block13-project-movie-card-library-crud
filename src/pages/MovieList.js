ecc147faacdf906a2b349568c3eebcbf64b2efcf
Christian Duessel <duessel@gmail.com>
2020-07-22 19:39:24 -0300

    some progress

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -3,6 +3,7 @@ import MovieCard from '../components/MovieCard';
 // import getmovies from '../services/movieAPI';
 import * as movieAPI from '../services/movieAPI';
 import Loading from '../components/Loading';
+import { Link } from 'react-router-dom';
 
 class MovieList extends Component {
   constructor(props) {
@@ -25,6 +26,7 @@ class MovieList extends Component {
     return (
       <div data-testid="movie-list">
         {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
+        <Link to="/movies/new">ADICIONAR CARTÃO</Link>
       </div>
     );
   }
