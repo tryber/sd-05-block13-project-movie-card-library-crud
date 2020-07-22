0d4371fd84edeb13f7f4ba1572727c6d7a925cbf
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-07-22 19:38:08 -0300

    CC stuff

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import * as movieAPI from '../services/movieAPI';
 import Loading from '../components/Loading';
-import { Link } from 'react-router-dom';
+
 
 class MovieDetails extends Component {
   constructor(props) {
@@ -27,9 +28,7 @@ class MovieDetails extends Component {
     // Change the condition to check the state
     const { movie, isLoading } = this.state;
     if (isLoading) return <Loading />;
-
     const { title, storyline, imagePath, genre, rating, subtitle } = movie;
-    
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={`../${imagePath}`} />
