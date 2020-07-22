58fa332632719fead600601423f2dd591c567652
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-07-22 16:34:31 -0300

    requirement 2 rendering MovieList

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import MovieList from './pages/MovieList';
 
 function App() {
   return (
     <BrowserRouter>
       <div>Movie Card Library CRUD</div>
+      <MovieList />
     </BrowserRouter>
   );
 }
