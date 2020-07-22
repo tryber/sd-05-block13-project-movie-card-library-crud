9b061311e034d73fdd832b95f674b97d55a46bd7
Rafael Quinteiro Leite <rafaelqleite@gmail.com>
2020-07-22 16:25:05 -0300

    item 3 implementado

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -2,9 +2,23 @@ import React from 'react';
 
 class MovieCard extends React.Component {
   render() {
+    const { title, storyline, imagePath, } = movie;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <div className="superior">
+          <img src={imagePath} alt={title} />
+          <div className="titulo">
+            {title}
+          </div>
+        </div>
+        <div className="inferior">
+          <div className="sinopse">
+            {storyline}
+          </div>
+          <div className="detalhes">
+            <Link to="/movies/:id">Ver Detalhes</Link>
+          </div>
+        </div>
       </div>
     );
   }
