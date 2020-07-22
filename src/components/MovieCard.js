f34e7ba3d2c21e8cce5522ff4006fc4a6107b22b
Lucas Costa Ventura <lucascv333@gmail.com>
2020-07-22 16:24:38 -0300

    corrigindo code climate

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -3,14 +3,14 @@ import React from 'react';
 
 class MovieCard extends React.Component {
   render() {
-    const { title, imagePath, storyline, id } = this.props.movie;
+    const { title, imagePath, storyline } = this.props.movie;
 
     return (
       <div data-testid="movie-card">
         <h2>{title}</h2>
         <img src={imagePath} alt={`Capa do filme ${title}`} />
         <span>{storyline}</span>
-        {/*<Link to={`movies/:${id}`}> VER DETALHES </Link>*/}
+        {/* <Link to={`movies/:${id}`}> VER DETALHES </Link> */}
       </div>
     );
   }
