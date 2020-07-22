5ee2a495e1509e881bc363846b8ae8fcc7fdef6c
marinabrcls <barcelos.marina@gmail.com>
2020-07-22 18:44:24 -0300

    setup router on app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import { Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { MovieList, MovieDetails, EditMovie, NewMovie, NotFound } from './pages/index';
 
 function App() {
   return (
+    <div>Movie Card Library CRUD</div>
     <Router>
-      <div>Movie Card Library CRUD</div>
+      <Switch>
+        <Route exact path="/" component={MovieList} />
+        <Route path="/movies/:id" component={MovieDetails} />
+        <Route path="/movies/:id/edit" component={EditMovie} />
+        <Route path="/movies/new" component={NewMovie} />
+        <Route path="" component={NotFound} />
+      </Switch>
     </Router>
   );
 }
