df3fd80c193964c003ccdd1b4971c99b5072b535
Rafael Assad <rafaassadeng@gmail.com>
2020-07-22 15:09:28 -0300

    3

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { MovieDetails, MovieList, EditMovie, NewMovie, NotFound } from './pages/index';
 
-
 function App() {
   return (
     <main>
       <Switch>
-        <Route exact path='/' component={MovieList} />
-        <Route path='/movies/:id' component={MovieDetails} />
-        <Route path='/movies/new' component={NewMovie} />
-        <Route path='/movies/:id/edit' component={EditMovie} />
+        <Route exact path="/" component={MovieList} />
+        <Route path="/movies/:id" component={MovieDetails} />
+        <Route path="/movies/new" component={NewMovie} />
+        <Route path="/movies/:id/edit" component={EditMovie} />
       </Switch>
     </main>
   );
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import movies from '../services/movieData';
 
 class MovieCard extends React.Component {
   render() {
+    const {
+      body: { title, subtitle, storyline, imagePath, id },
+    } = this.props;
     return (
       <div data-testid="movie-card">
         Movie Card
+        <div>{title}</div>
+        <div>{subtitle}</div>
+        <div>{storyline}</div>
+        <img src={imagePath} alt={title} />
+        <div>
+          <Link to={`movies/${id}`}>VER DETALHES<Link/>
+        </div>
       </div>
     );
   }
