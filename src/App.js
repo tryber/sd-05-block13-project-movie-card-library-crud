2391175d2a06362f022627ce8e00acf95fdbadc4
Jorge Lucas <jorge.lucas2264@gmail.com>
2020-07-22 19:24:24 -0300

    CineJorgin project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
+import { EditMovie, MovieDetails, MovieList, NewMovie, NotFound } from './pages/index';
 
 function App() {
   return (
-      <div>Movie Card Library CRUD</div>
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" Component={MovieList} />
+          <Route path="/movies/:id" Component={MovieDetails} />
+          <Route path="/movies/new" Component={NewMovie} />
+          <Route path="/movies/:id/edit" Component={EditMovie} />
+        </Switch>
+      </BrowserRouter>
+    </div>
   );
 }
 
