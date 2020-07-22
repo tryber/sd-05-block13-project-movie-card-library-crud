78d4023bb0c8cff9467a9a3e57e0677344a28005
Paulo-Dandrea <prog.dandrea@gmail.com>
2020-07-22 18:13:49 -0300

    editing and submiting working

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import MovieList from './pages/MovieList';
 import MovieDetails from './pages/MovieDetails';
+import EditMovie from './pages/EditMovie';
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <div>Movie Card Library CRUD</div>
         <Switch>
           {/* <Route path='/movies/:id' component={MovieDetails} /> */}
-          {/* <Route path='/movies/:id/edit' component={<div>alo</div>} /> */}
+          <Route path='/movies/:id/edit' component={EditMovie} />
           <Route path='/movies/:id' component={MovieDetails} />
           <Route path='/' component={MovieList} />
           {/* <MovieList /> */}
--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,30 +1,51 @@
 import React, { Component } from 'react';
 
-import { MovieForm } from '../components';
+import { MovieForm, Loading } from '../components';
 import * as movieAPI from '../services/movieAPI';
+import { Redirect } from 'react-router-dom';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      movie: [],
+      shouldRedirect: false
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.id = this.props.match.params.id;
+
   }
 
-  handleSubmit(updatedMovie) {
+ async handleSubmit(updatedMovie) {
+    await movieAPI.updateMovie(updatedMovie);
+    this.setState({
+      shouldRedirect: true,
+    })
+  }
+
+  async componentDidMount() {
+    const response = await movieAPI.getMovie(this.id);
+    this.setState({
+      movie: response
+    })
   }
 
   render() {
     const { status, shouldRedirect, movie } = this.state;
+    // {loggedIn ? <Redirect to="/profile" /> : <HomePage />}
     if (shouldRedirect) {
       // Redirect
+      return <Redirect to='/' />
     }
 
-    if (status === 'loading') {
+    if (this.state.movie.length === 0) {
       // render Loading
+      return <Loading />
     }
 
     return (
       <div data-testid="edit-movie">
+        {console.log(this.props.match.params.id)}
         <MovieForm movie={movie} onSubmit={this.handleSubmit} />
       </div>
     );
@@ -32,3 +53,6 @@ class EditMovie extends Component {
 }
 
 export default EditMovie;
+
+// EditMovie: deve realizar uma requisição para buscar o filme que será editado.
+// Ao ser montada, a página de edição do filme deve fazer uma requisição pra buscar o filme que será editado e deve, ao ter seu formulário submetido, artualizar o filme e redirecionar a página pra rota raíz.
