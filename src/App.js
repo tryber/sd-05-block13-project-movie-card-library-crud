7cb499298684b11a937cd9d2c7edcdc0d93ea7ee
Christian Duessel <duessel@gmail.com>
2020-07-22 16:32:06 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Link, Route } from 'react-router-dom';
+import './App.css';
+import Index from './pages/index';
+import EditMovie from './pages/EditMovie';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import NotFound from './pages/NotFound';
+
+
 
 function App() {
   return (
+    <Router>
       <div>Movie Card Library CRUD</div>
+      <main>
+        <Switch>
+          <Route path="/movies/:id/edit" component={EditMovie} />
+          <Route path="/movies/:id" component={MovieDetails} />
+          <Route path="/movies/new" component={NewMovie} />
+          <Route path="/not-found" component={NotFound} />
+          <Route exact path="/" component={Index} />
+        </Switch>
+      </main>
+      <footer>
+        <h3>made with love by CD</h3>
+      </footer>
+    </Router>
   );
 }
 
