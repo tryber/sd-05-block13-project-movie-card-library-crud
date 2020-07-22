5a2bd1f61d317ace3a19487778bf428c52243fff
Lucas Costa Ventura <lucascv333@gmail.com>
2020-07-22 16:30:24 -0300

    add BrowserRouter dentro de index.js

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,16 +1,16 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
-    const { title, imagePath, storyline } = this.props.movie;
+    const { title, imagePath, storyline, id } = this.props.movie;
 
     return (
       <div data-testid="movie-card">
         <h2>{title}</h2>
         <img src={imagePath} alt={`Capa do filme ${title}`} />
         <span>{storyline}</span>
-        {/* <Link to={`movies/:${id}`}> VER DETALHES </Link> */}
+        <Link to={`movies/:${id}`}> VER DETALHES </Link> */
       </div>
     );
   }
