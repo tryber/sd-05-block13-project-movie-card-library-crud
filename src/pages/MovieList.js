169a979a2979787b6ad1d552d6b1c16d1ea6cae0
Mau <mauricio.sdjr@gmail.com>
2020-07-23 10:23:22 -0300

    implement Movielist API requisition

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,17 +1,35 @@
-import React, { Component } from 'react';
-import MovieCard from '../components/MovieCard';
+import React, { Component } from "react";
+import MovieCard from "../components/MovieCard";
+import Loading from "../components/Loading";
 
-import * as movieAPI from '../services/movieAPI';
+import * as movieAPI from "../services/movieAPI";
 
 class MovieList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: '',
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    movieAPI
+      .getMovies()
+      .then((response) => this.setState({ movies: response, loading: false }));
+  }
+
   render() {
-    const { movies } = this.state;
+    const { movies, loading } = this.state;
 
     // Render Loading here if the request is still happening
+    if (loading) return <Loading />;
 
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
+        {movies.map((movie) => (
+          <MovieCard key={movie.title} movie={movie} />
+        ))}
       </div>
     );
   }
