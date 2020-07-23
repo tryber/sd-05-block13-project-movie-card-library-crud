5262e8270b3acda33a8a1b60e334dbfc3643607a
Eder Sena <eder.sena@live.com>
2020-07-23 00:26:35 -0300

    movie list

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,25 +1,72 @@
+mport PropTypes from 'prop-types';
 import React, { Component } from 'react';
-
+import { Link, Redirect } from 'react-router-dom';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      movie: {
+        title: '',
+        storyline: '',
+        imagePath: '',
+        genre: '',
+        rating: 0,
+        subtitle: '',
+        id: '',
+      },
+      isLoaded: false,
+      shouldRiderict: false,
+    };
+  }
+
+  componentDidMount() {
+    const { match } = this.props;
+    movieAPI.getMovie(match.params.id).then((movie) => {
+      const isLoaded = true;
+      this.setState({ movie, isLoaded });
+    });
+  }
+
   render() {
-    // Change the condition to check the state
-    if (true) return <Loading />;
-
-    const { title, storyline, imagePath, genre, rating, subtitle } = movie;
-
-    return (
-      <div data-testid="movie-details">
-        <img alt="Movie Cover" src={`../${imagePath}`} />
-        <p>{`Subtitle: ${subtitle}`}</p>
-        <p>{`Storyline: ${storyline}`}</p>
-        <p>{`Genre: ${genre}`}</p>
-        <p>{`Rating: ${rating}`}</p>
-      </div>
-    );
+    const { movie, isLoaded, shouldRiderict } = this.state;
+    if (shouldRiderict) return <Redirect to="/" />;
+    if (isLoaded) {
+      const { title, storyline, imagePath, genre, rating, subtitle, id } = movie;
+      const { match } = this.props;
+      return (
+        <div data-testid="movie-details">
+          <Link to="/">VOLTAR</Link>
+          <img alt="Movie Cover" src={`../${imagePath}`} />
+          <p>{`Title: ${title}`}</p>
+          <p>{`Subtitle: ${subtitle}`}</p>
+          <p>{`Storyline: ${storyline}`}</p>
+          <p>{`Genre: ${genre}`}</p>
+          <p>{`Rating: ${rating}`}</p>
+          <Link to={`/movies/${id}/edit`}>EDITAR</Link>
+          <Link
+            to="/"
+            onClick={() => {
+              movieAPI.deleteMovie(id);
+              this.setState({ shouldRiderict: true });
+            }}
+          >
+            DELETAR
+          </Link>
+        </div>
+      );
+    }
+    return <Loading />;
   }
 }
 
 export default MovieDetails;
+
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    id: PropTypes.number,
+  }).isRequired,
+};
