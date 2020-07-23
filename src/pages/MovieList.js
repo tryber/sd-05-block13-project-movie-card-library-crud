fd842ad2b381c0bbee51a4701f2eda87b40a6164
César Alves <alvescesar@hotmail.com>
2020-07-22 22:45:01 -0300

    'MovieList' requirements 1st try

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -2,13 +2,33 @@ import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
 
 import * as movieAPI from '../services/movieAPI';
+import { Loading } from '../components';
+import { NotFound } from '../components';
 
 class MovieList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.setState({
+      movies: [],
+      loading: true,
+      error: false,
+    });
+  }
+
+  componentDidMount() {
+    movieAPI.getMovie()
+      .then((movie) => this.setState({
+        movies: movie,
+        loading: false,
+      }));
+  }
+
   render() {
     const { movies } = this.state;
 
-    // Render Loading here if the request is still happening
-
+    if (this.state.loading) return <Loading />;
+    if (this.state.error) return <NotFound />;
     return (
       <div data-testid="movie-list">
         {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
