143cd2e53505dfe65068ab23bd9896d0e36d1509
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-07-23 13:40:16 -0300

    initiating req.5 EditMovie

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import MovieList from './pages/MovieList';
 import MovieDetails from './pages/MovieDetails';
+import EditMovie from './pages/EditMovie';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={MovieList} />
         <Route path="/movies/:id" component={MovieDetails} />
+        <Route path="/movies/:id/edit" component={EditMovie} />
       </Switch>
     </BrowserRouter>
   </section>
