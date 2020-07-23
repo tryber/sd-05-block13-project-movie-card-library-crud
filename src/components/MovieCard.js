32f3c0caf417a8b17641b86197ef91a067f25ef6
pedrohcalado <pedrocalado22@gmail.com>
2020-07-23 09:03:10 -0300

    code climate

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
@@ -13,5 +14,12 @@ class MovieCard extends React.Component {
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.object,
+  title: PropTypes.string,
+  storyline: PropTypes.string,
+  id: PropTypes.number,
+};
 
 export default MovieCard;
