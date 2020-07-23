d8515440a5dd5b9f158472fa2a1393a38e8eba3d
Lucas Costa Ventura <lucascv333@gmail.com>
2020-07-23 00:07:47 -0300

    trocando BrowserRouter por Router e incluindo page NotFound dentro das rotas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import MovieList from './pages/MovieList';
 import MovieDetails from './pages/MovieDetails';
 import NewMovie from './pages/NewMovie';
 import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
@@ -11,14 +12,15 @@ function App() {
       <h1>Movie Card Library CRUD</h1>
 
       <main>
-        <BrowserRouter>
+        <Router>
           <Switch>
-            <Route exact path="/" component={MovieList} />
             <Route path="/movies/new" component={NewMovie} />
             <Route path="/movies/:id/edit" component={EditMovie} />
             <Route path="/movies/:id" component={MovieDetails} />
+            <Route exact path="/" component={MovieList} />
+            <Route component={NotFound} />
           </Switch>
-        </BrowserRouter>
+        </Router>
       </main>
 
     </div>
