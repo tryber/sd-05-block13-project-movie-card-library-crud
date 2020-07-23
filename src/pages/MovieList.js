b5d5a500c19f609e0c5da49c1d5e33e01d7f0d28
Mau <mauricio.sdjr@gmail.com>
2020-07-23 10:26:34 -0300

    codeclimate

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,8 +1,8 @@
-import React, { Component } from "react";
-import MovieCard from "../components/MovieCard";
-import Loading from "../components/Loading";
+import React, { Component } from 'react';
+import MovieCard from '../components/MovieCard';
+import Loading from '../components/Loading';
 
-import * as movieAPI from "../services/movieAPI";
+import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
   constructor(props) {
@@ -22,7 +22,6 @@ class MovieList extends Component {
   render() {
     const { movies, loading } = this.state;
 
-    // Render Loading here if the request is still happening
     if (loading) return <Loading />;
 
     return (
