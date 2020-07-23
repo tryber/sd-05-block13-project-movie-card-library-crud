6c67e257e16bf5460221a5e744755cb471c2f3bb
pedrohcalado <pedrocalado22@gmail.com>
2020-07-23 08:29:29 -0300

    Item 2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import MovieList from './pages/MovieList';
 
 function App() {
   return (
+    <BrowserRouter>
       <div>Movie Card Library CRUUD</div>
+      <MovieList />
+    </BrowserRouter>
   );
 }
 
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
-
+import Loading from '../components/Loading'
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: '',
+      isLoaded: false,
+    };
+  }
+
+  componentDidMount() {
+    movieAPI.getMovies()
+      .then(response =>{
+        // console.log(response);
+        this.setState({
+          movies: response,
+          isLoaded: true,
+        })
+        // console.log(this.state.movies);
+        })
+  }
+
   render() {
     const { movies } = this.state;
-
+    // console.log(movies)
     // Render Loading here if the request is still happening
-
+    if (!this.state.isLoaded) return (<Loading />);
     return (
       <div data-testid="movie-list">
         {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
