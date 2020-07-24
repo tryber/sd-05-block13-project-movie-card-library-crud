60ce06d16d56090898e2acff2195615b37dff978
Mau <mauricio.sdjr@gmail.com>
2020-07-24 10:13:33 -0300

    implement MovieCard component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import MovieList from './pages/MovieList';
+import {BrowserRouter} from 'react-router-dom';
+import {Switch, Route, Link} from 'react-router-dom';
+import NewMovie from './pages/NewMovie'
+import MovieDetails from './pages/MovieDetails';
+import EditMovie from './pages/EditMovie'
 
 function App() {
   return (
-      <div>Movie Card Library CRUD</div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <div>Movie Card Library CRUD</div>
+        </header>
+        <Switch>
+          <Route exact path="/" component={MovieList} />
+          <Route path="/movies/:id" component={MovieDetails} />
+          <Route path="/movies/new" component={NewMovie} />
+          <Route path="/movies/:id/edit" component={EditMovie} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -2,21 +2,39 @@ import React, { Component } from 'react';
 
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
+import {Link} from 'react-router-dom';
 
 class MovieDetails extends Component {
-  render() {
-    // Change the condition to check the state
-    if (true) return <Loading />;
+  constructor(props) {
+    super(props);
+    this.state = {
+      movie: '',
+      loading: true,
+    };
+  }
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = movie;
+  componentDidMount() {
+    const id = this.props.match.params.id;
+    movieAPI.getMovie(id).then((response) => this.setState({movie: response, loading: false}))
+  }
+
+  render() {
+    const { loading, movie } = this.state;
+
+    if (loading) return (<Loading />);
+
+    const { title, storyline, imagePath, genre, rating, subtitle, id } = movie;
 
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={`../${imagePath}`} />
+        <p>{`Title: ${title}`}</p>
         <p>{`Subtitle: ${subtitle}`}</p>
         <p>{`Storyline: ${storyline}`}</p>
         <p>{`Genre: ${genre}`}</p>
         <p>{`Rating: ${rating}`}</p>
+        <Link to={`/movies/${id}/edit`}>EDITAR</Link>
+        <Link to="/">VOLTAR</Link>
       </div>
     );
   }
