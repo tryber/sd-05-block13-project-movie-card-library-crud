a3ebfb710ba77a6a7357b25c650d6cc98edececa
Mau <mauricio.sdjr@gmail.com>
2020-07-24 13:07:52 -0300

    implement Edit Movie component

--- src/App.js
+++ src/App.js
@@ -14,9 +14,9 @@ function App() {
         </header>
         <Switch>
           <Route exact path="/" component={MovieList} />
-          <Route path="/movies/:id" component={MovieDetails} />
           <Route path="/movies/new" component={NewMovie} />
           <Route path="/movies/:id/edit" component={EditMovie} />
+          <Route path="/movies/:id" component={MovieDetails} />
         </Switch>
       </div>
     </BrowserRouter>
--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,26 +1,37 @@
 import React, { Component } from 'react';
 
-import { MovieForm } from '../components';
+import { MovieForm, Loading } from '../components';
 import * as movieAPI from '../services/movieAPI';
+import { Redirect } from 'react-router-dom';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      status: 'loading',
+      shouldRedirect: false,
+      movie: '',
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  componentDidMount() {
+    const id = this.props.match.params.id;
+    movieAPI.getMovie(id).then((response) => this.setState({ movie: response, status: 'mounted' }));
+  }
+
   handleSubmit(updatedMovie) {
+   movieAPI.updateMovie(updatedMovie).then(() => this.setState({ shouldRedirect: true }));         
   }
 
   render() {
     const { status, shouldRedirect, movie } = this.state;
     if (shouldRedirect) {
-      // Redirect
+      return (<Redirect to="/" />)
     }
 
     if (status === 'loading') {
-      // render Loading
+      return (<Loading />)
     }
 
     return (
