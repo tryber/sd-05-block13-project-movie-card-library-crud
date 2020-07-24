6c40398bd573a7ebaa03865c8266563784340402
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-07-24 11:40:46 -0300

    requirement 6 NewMovie on progress

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import MovieList from './pages/MovieList';
 import MovieDetails from './pages/MovieDetails';
 import EditMovie from './pages/EditMovie';
+import NewMovie from './pages/NewMovie';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route exact path="/" component={MovieList} />
         <Route exact path="/movies/:id" component={MovieDetails} />
         <Route path="/movies/:id/edit" component={EditMovie} />
+        <Route path="/movies/new" component={NewMovie} />
       </Switch>
     </BrowserRouter>
   </section>
--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 
 import MovieForm from '../components/MovieForm';
 import * as movieAPI from '../services/movieAPI';
+import Loading from '../components/Loading';
 
 class NewMovie extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isLoaded: false,
+      shouldRedirect: false,
+    }
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  componentDidMount() {
+    this.setState({
+      isLoaded: true,
+    });
+  }
+
   handleSubmit(newMovie) {
+    movieAPI.createMovie(newMovie)
+    .then(() => this.setState({
+      shouldRedirect: true,
+    }));
   }
 
   render() {
+
+    const { isLoaded, shouldRedirect } = this.state;
+    if (shouldRedirect) return <Redirect to="/" />;
+    if (isLoaded) return <Loading />;
     return (
       <div data-testid="new-movie">
         <MovieForm onSubmit={this.handleSubmit} />
