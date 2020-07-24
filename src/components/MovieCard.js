01c408b00d24ac68948f69ad399db27dad27aff4
RenataNovais <rnovaiscs@gmail.com>
2020-07-24 12:50:05 -0300

    using prop-types

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
-    const { title, storyline, imagePath } = this.props.movie;
+    const { title, storyline, imagePath, id } = this.props.movie;
     return (
       <div data-testid="movie-card">
         <div className="card">
-          <img src={imagePath} alt='Movie Cover' />
+          <img src={imagePath} alt="Movie Cover" />
           <h4>{title}</h4>
           <p>{storyline}</p>
-          <Link to='/movies/:id'>VER DETALHES</Link>
+          <Link to={`/movies/:${id}`}>VER DETALHES</Link>
         </div>
       </div>
     );
   }
+}
+
+MovieCard.propTypes = {
+  title: PropTypes.string,
+  storyline: PropTypes.string,
+  imagePath: PropTypes.string,
+  id: PropTypes.number,
 }
 
 export default MovieCard;
