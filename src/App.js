55e11e26c57ba121f6bdc87305ea3e5e77c878cf
marcosjuniorclimaco <markojuniorclimako@gmail.com>
2020-07-24 14:28:18 -0300

    iniciando projeto cc

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import './App.css';
+import { BrowserRouter as Router } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
