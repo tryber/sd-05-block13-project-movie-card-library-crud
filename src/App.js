23a2d766aa22014a68d9c5ef401a9e5ba88e3314
Natalia Macedo <nataliasilvamacedo@gmail.com>
2020-07-24 11:59:01 -0300

    Mudança das rotas no App

--- src/App.js
+++ src/App.js
@@ -6,13 +6,12 @@ import { MovieList, MovieDetails, EditMovie, NewMovie } from './pages';
 function App() {
   return (
     <Router>
-      <MovieList />
       <div>Movie Card Library CRUD</div>
       <Switch>
         <Route exact path="/" component={MovieList} />
-        <Route path="/movies/:id" component={MovieDetails} />
         <Route path="/movies/new" component={NewMovie} />
         <Route path="movies/:id/edit" component={EditMovie} />
+        <Route path="/movies/:id" component={MovieDetails} />      
       </Switch>
     </Router>
   );
