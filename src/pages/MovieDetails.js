d58374c4315fe1939d35466d82379f56df323f8d
RenataNovais <rnovaiscs@gmail.com>
2020-07-24 14:35:52 -0300

    cc

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import { Loading } from '../components';
-import { Link } from 'react-router-dom';
+import BrowserRoute, { Link } from 'react-router-dom';
 import * as movieAPI from '../services/movieAPI';
 
 class MovieDetails extends Component {
