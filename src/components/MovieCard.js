f27f621477a210e9c424225d2e8262523a220b24
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-07-24 13:14:13 -0300

    adding image to root page

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -5,10 +5,10 @@ import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
-    const { title, storyline, id } = this.props.movie;
+    const { imagePath, title, storyline, id } = this.props.movie;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <img src={imagePath} />
         <div>{title}</div>
         <div>{storyline}</div>
         <Link to={`/movies/${id}`}>VER DETALHES</Link>
