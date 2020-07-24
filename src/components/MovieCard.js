1d2b09605066ad408e99561de6fef1a07b035e13
Rafael Assad <rafaassadeng@gmail.com>
2020-07-24 11:36:35 -0300

    types

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 // import movies from '../services/movieData';
 
 class MovieCard extends React.Component {
@@ -21,3 +22,13 @@ class MovieCard extends React.Component {
 }
 
 export default MovieCard;
+
+MovieCard.propTypes = {
+  body: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
+    storyline: PropTypes.string.isRequired,
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
