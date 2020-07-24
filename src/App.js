33c2bdf87067a813380834a23bf6e07288c9b88d
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-07-24 13:25:46 -0300

    added NotFound to complete requirement1

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import MovieList from './pages/MovieList';
 import MovieDetails from './pages/MovieDetails';
 import EditMovie from './pages/EditMovie';
 import NewMovie from './pages/NewMovie';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/movies/new" component={NewMovie} />
         <Route path="/movies/:id/edit" component={EditMovie} />
         <Route exact path="/movies/:id" component={MovieDetails} />
+        <Route path="/:error" component={NotFound} />
       </Switch>
     </BrowserRouter>
   </section>
