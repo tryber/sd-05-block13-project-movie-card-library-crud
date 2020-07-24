66e39bcd6cae5e4e708b76c998527fcf17fecaf3
Christian Duessel <duessel@gmail.com>
2020-07-23 22:15:02 -0300

    agora vai!

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -24,7 +24,7 @@ class MovieList extends Component {
       return <Loading />;
     }
     return (
-      <div></div>
+      <div>
         <div data-testid="movie-list">
           {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
         </div>
