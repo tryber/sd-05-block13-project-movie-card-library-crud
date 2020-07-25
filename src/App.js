f40d852c138222b668eb07e90e84f28e98daa3f0
marinabrcls <barcelos.marina@gmail.com>
2020-07-25 13:07:35 -0300

    altera app.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { MovieList, MovieDetails, EditMovie, NewMovie, NotFound } from './pages/index';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './App.css';
+import { MovieList, MovieDetails, EditMovie, NewMovie, NotFound } from './pages';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
     <Router>
+      <div>Movie Card Library CRUD</div>
       <Switch>
         <Route exact path="/" component={MovieList} />
-        <Route path="/movies/:id" component={MovieDetails} />
-        <Route path="/movies/:id/edit" component={EditMovie} />
         <Route path="/movies/new" component={NewMovie} />
-        <Route component={NotFound} />
+        <Route path="/movies/:id/edit" component={EditMovie} />
+        <Route path="/movies/:id" component={MovieDetails} />
+        <Route path="/:error" component={NotFound} />
       </Switch>
     </Router>
   );
