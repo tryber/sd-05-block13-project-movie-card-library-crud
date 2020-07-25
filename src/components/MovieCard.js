20653b68c6c62f2d3eef49410732269f3ef731f6
marinabrcls <barcelos.marina@gmail.com>
2020-07-25 13:03:37 -0300

    improved movie card

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,27 +1,34 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
-    const { imagePath, title, storyline, id } = this.props.movie;
+    const { title, imagePath, subtitle, storyline, genre, rating, id, } = this.props.movie;
     return (
       <div data-testid="movie-card">
-        <img src={imagePath} alt={title} />
-        <div>{title}</div>
-        <div>{storyline}</div>
-        <div><Link to={`/movies/${id}`}>VER DETALHES</Link></div>
+        <h1>{title}</h1>
+        <img alt="Movie Cover" src={`../${imagePath}`} />
+        <p>{subtitle}</p>
+        <p>{storyline}</p>
+        <p>{genre}</p>
+        <p>{rating}</p>
+        <Link to={`/movies/${id}`}>VER DETALHES</Link>
       </div>
     );
-  }
-  PropTypes = {
-    movie: PropTypes.shape({
-      imagePath: PropTypes.string.isRequired,
-      title: PropTypes.string.isRequired,
-      storyline: PropTypes.string.isRequired,
-      id: PropTypes.number.isRequired,
-    }).isRequired
   }
 }
 
 export default MovieCard;
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    subtitle: PropTypes.string.isRequired,
+    genre: PropTypes.string.isRequired,
+    imagePath: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+    id: PropTypes.number.isRequired,
+  }).isRequired,
+};
