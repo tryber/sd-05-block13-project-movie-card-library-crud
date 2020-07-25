c16307e5c783966474d8b4deeb426a5781174bae
Andrevlc <andrevezi@gmail.com>
2020-07-25 19:02:02 -0300

    Commit 1 - skipping first requirement and finalizing MovieList with comments

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
+import MovieList from './pages/MovieList';
 
 function App() {
   return (
     <Router>
       <div>Movie Card Library CRUD</div>
+      <MovieList />
     </Router>
   );
 }
