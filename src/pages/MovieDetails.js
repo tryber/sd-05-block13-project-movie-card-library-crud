7d0e194b49fce94c4d733558b08eab6f8e5514a0
labelle <beelsdelonge@gmail.com>
2020-07-25 17:52:36 -0300

    MovieDetails com Imports e CC

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,25 +1,36 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
-import { Link } from 'react-router-dom';
+import NotFound from '../pages/NotFound';
 
 class MovieDetails extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      movie: '',
+      movies: '',
       loading: true,
       error: false,
     };
-  };  
-  
+  }
+
+  componentDidMount() {
+    movieAPI.getMovie(this.props.match.params.id)
+      .then((movie) => this.setState({
+        movies: movie,
+        loading: false,
+      }));
+  }
+
   render() {
+    const { movies, loading, error } = this.state;
     if (loading) return <Loading />;
     if (error) return <NotFound />;
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = movie;
+    const { title, storyline, imagePath, genre, rating, subtitle, id } = movies;
 
     return (
       <div data-testid="movie-details">
@@ -37,3 +48,11 @@ class MovieDetails extends Component {
 }
 
 export default MovieDetails;
+
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
