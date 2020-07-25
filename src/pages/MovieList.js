4b88b6a5425a5d787e602fe148eddb2bc75d66cd
marinabrcls <barcelos.marina@gmail.com>
2020-07-25 13:53:13 -0300

    initial version of movieList

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,17 +1,35 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import MovieCard from '../components/MovieCard';
-
+import Loading from '../components/Loading';
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: [],
+      loading: false,
+    };
+  }
+
+  componentDidMount() {
+    movieAPI.getMovies().then((response) => {
+      this.setState({
+        loading: true,
+        movies: response,
+      });
+    });
+  }
+
   render() {
-    const { movies } = this.state;
-
-    // Render Loading here if the request is still happening
-
+    const { movies, loading } = this.state;
+    if (!loading) return <Loading />;
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
+        {movies.map((movie) =>
+          <MovieCard key={movie.title} movie={movie} />
+        )}
       </div>
     );
   }
