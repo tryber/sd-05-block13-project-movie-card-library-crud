89599ff89cac25b4c79812c7802e3c551834d05b
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-07-25 10:19:10 -0300

    deleteMovie functional

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import * as movieAPI from '../services/movieAPI';
@@ -12,7 +12,9 @@ class MovieDetails extends Component {
     this.state = {
       movie: {},
       isLoading: true,
+      shouldRedirect: false,
     };
+    this.deleteMovie = this.deleteMovie.bind(this);
   }
 
   componentDidMount() {
@@ -25,10 +27,17 @@ class MovieDetails extends Component {
     }));
   }
 
+  deleteMovie() {
+    const idMovie = this.state.movie.id;
+    movieAPI.deleteMovie(idMovie);
+    this.setState ({ shouldRedirect: true });
+  }
+
   render() {
     // Change the condition to check the state
-    const { movie, isLoading } = this.state;
+    const { movie, isLoading, shouldRedirect } = this.state;
     if (isLoading) return <Loading />;
+    if (shouldRedirect) return <Redirect to="/" />;
     const { title, storyline, imagePath, genre, rating, subtitle } = movie;
     return (
       <div data-testid="movie-details">
@@ -38,8 +47,9 @@ class MovieDetails extends Component {
         <p>{`Storyline: ${storyline}`}</p>
         <p>{`Genre: ${genre}`}</p>
         <p>{`Rating: ${rating}`}</p>
-        <Link to={`/movies/${this.props.match.params.id}/edit`}>EDITAR</Link>
-        <Link to="/">VOLTAR</Link>
+        <Link to={`/movies/${this.props.match.params.id}/edit`}>EDITAR</Link><br />
+        <Link to="/">VOLTAR</Link><br />
+        <Link to="/" onClick={this.deleteMovie}>DELETAR esse filme</Link>
       </div>
     );
   }
