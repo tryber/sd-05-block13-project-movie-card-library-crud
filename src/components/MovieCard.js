bbe54646af76b1657746db127e437b0140547c85
Andrevlc <andrevezi@gmail.com>
2020-07-25 23:34:25 -0300

    Commit 4 - Checked movielist and added info at movieCard, no style added yet

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
+
   render() {
+    const { title, storyline, id } = this.props.movie;
     return (
       <div data-testid="movie-card">
         Movie Card
+        <div>{title}</div>
+        <div>{storyline}</div>
+        <Link to={`/movies/${id}`}>VER DETALHES</Link>
       </div>
     );
   }
 }
+// Testa se os tipos estão corretos, serve para resolver o
+// problema abaixo:
+// 'movie.title' is missing in props validation
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default MovieCard;
