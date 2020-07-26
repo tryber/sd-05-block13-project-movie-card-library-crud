e30333d3ab8a86ab1011d29aa33320668160d566
Flavianny Maciel <flaviannymaciel@gmail.com>
2020-07-26 14:53:45 -0300

    bora bora

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
+// import MovieList from './pages/MovieList';
+// import MovieDetails from './pages/MovieDetails';
 
 function App() {
   return (
     <Router>
-      <div>Movie Card Library CRUD</div>
+      {/* <Switch>
+        <Route exact path="/" component ={MovieList} />
+        <Route path="/movies/:id" component={MovieDetails} />
+      </Switch> */}
     </Router>
   );
 }
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
-
+import Loading from '../components/Loading';
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: null,
+      isLoading:true,
+    }
+  }
+  componentDidMount() {
+    movieAPI.getMovies().then((arrMovies) => this.setState({
+      movies: arrMovies,
+      isLoading: false,
+    }))
+  }
   render() {
-    const { movies } = this.state;
-
-    // Render Loading here if the request is still happening
-
+    const { movies, isLoading } = this.state;
+    if(isLoading) return <Loading /> 
     return (
       <div data-testid="movie-list">
         {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
