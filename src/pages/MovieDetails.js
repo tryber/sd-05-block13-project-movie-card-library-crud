8d16925fdd8167287d42dcf5df7142cf5651414a
Flavianny Maciel <flaviannymaciel@gmail.com>
2020-07-26 15:45:42 -0300

    agora vai cc

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
-import PropTypes from 'prop-types';
 
 class MovieDetails extends Component {
   constructor(props) {
@@ -20,10 +21,9 @@ class MovieDetails extends Component {
       }));
   }
   render() {
-    // Change the condition to check the state
-    const { isLoading, movie } = this.state;
-    if (isLoading) return <Loading />;
-    const { title, storyline, imagePath, genre, rating, subtitle, id } = movie;
+  const { isLoading, movie } = this.state;
+  if (isLoading) return <Loading />;
+  const { title, storyline, imagePath, genre, rating, subtitle, id } = movie;
  
     return (
       <div data-testid="movie-details">
