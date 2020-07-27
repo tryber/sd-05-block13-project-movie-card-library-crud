527dea2b23a7e1b48ec76718c9bb833a59bcb9ae
RenataNovais <rnovaiscs@gmail.com>
2020-07-27 13:35:04 -0300

    trying.......

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 import { MovieDetails, MovieList, EditMovie, NewMovie, NotFound } from './pages/index';
 
 function App() {
@@ -8,11 +8,13 @@ function App() {
       <div>Movie Card Library CRUD</div>
       <Router>
         <Link to="/movies/new">ADICIONAR CARTÃO</Link>
-        <Route exact path="/movies/new" component={NewMovie} />
-        <Route exact path="/" component={MovieList} />
-        <Route exact path="/movies/:id/edit" component={EditMovie} />
-        <Route exact path="/movies/:id" component={MovieDetails} />
-        <Route exact path="/404-error" component={NotFound} />
+        <Switch>
+          <Route exact path="/" component={MovieList} />
+          <Route exact path="/movies/new" component={NewMovie} />
+          <Route exact path="/movies/:id/edit" component={EditMovie} />
+          <Route exact path="/movies/:id" component={MovieDetails} />
+          <Route component={NotFound} />
+        </Switch>
       </Router>
     </div>
   );
