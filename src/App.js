00a19997d145f05cf7331c084d4c5427141fde62
Lucas Staroscky <LucasStaroscky@MacBook-Pro-de-Lucas.local>
2020-07-26 21:26:39 -0300

    fourth requirement completed code climate issues - props validation

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ function App() {
     <Router>
       <h1>Movie Cards Library CRUD</h1>
       <Switch>
-        <Route exact path="/movies/:id" component={MovieDetails} />
         <Route exact path="/" component={MovieList} />
         <Route exact path="/" component={MovieDetails} />
+        <Route exact path="/movies/:id" component={MovieDetails} />
         <Route exact path="/movies/new" component={NewMovie} />
         <Route exact path="/movies/:id/edit" component={EditMovie} />
       </Switch>
