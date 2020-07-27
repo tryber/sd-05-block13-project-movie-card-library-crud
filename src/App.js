2bffdcdd4e5c4ad885610363c5f6aeb7038f4794
Mau <mauricio.sdjr@gmail.com>
2020-07-27 00:59:40 -0300

    add link from NewMovie to home and from home to NewMovie and implement handlesubmit function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import MovieList from './pages/MovieList';
 import NewMovie from './pages/NewMovie';
 import MovieDetails from './pages/MovieDetails';
@@ -15,7 +15,7 @@ function App() {
         </header>
         <Switch>
           <Route exact path="/" component={MovieList} />
-          <Route path="/movies/new" component={NewMovie} />
+          <Route exact path="/movies/new" component={NewMovie} />
           <Route path="/movies/:id/edit" component={EditMovie} />
           <Route path="/movies/:id" component={MovieDetails} />
           <Route component={NotFound} />
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import MovieCard from '../components/MovieCard';
 import Loading from '../components/Loading';
 
 import * as movieAPI from '../services/movieAPI';
+
 
 class MovieList extends Component {
   constructor(props) {
@@ -25,10 +27,13 @@ class MovieList extends Component {
     if (loading) return (<Loading />);
 
     return (
-      <div data-testid="movie-list">
-        {movies.map((movie) => (
-          <MovieCard key={movie.title} movie={movie} />
-        ))}
+      <div>
+        <div data-testid="movie-list">
+          {movies.map((movie) => (
+          <MovieCard key={movie.title}  movie={movie} />
+          ))}
+        </div>
+        <Link to="/movies/new">ADICIONAR CARTÃO</Link>
       </div>
     );
   }
