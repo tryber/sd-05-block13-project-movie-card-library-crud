a885d124d0edd20a55bd3e2d56473c0993b0a6b1
Mau <mauricio.sdjr@gmail.com>
2020-07-27 14:06:15 -0300

    remove warning putting prop inside MovieForm in NewMovie

--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -22,7 +22,16 @@ class NewMovie extends Component {
     if (redirect) return (<Redirect to="/" />);
     return (
       <div data-testid="new-movie">
-        <MovieForm onSubmit={this.handleSubmit} />
+        <MovieForm movie={{
+          id: '',
+          title: '',
+          subtitle: '',
+          storyline: '',
+          rating: '',
+          imagePath: '',
+          bookmarked: '',
+          genre: '',}} 
+        onSubmit={this.handleSubmit} />
       </div>
     );
   }
