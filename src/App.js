50ac33beeb7f2ab6ad046dbb4f8bb8b5ac6f4071
pauloricardoz <trybe.przt@gmail.com>
2020-07-27 13:41:34 -0300

    100% with error page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import MovieList from './pages/MovieList';
 import MovieDetails from './pages/MovieDetails';
 import EditMovie from './pages/EditMovie';
 import NewMovie from './pages/NewMovie';
 import NotFound from './pages/NotFound';
+import './App.css';
 
 function App() {
   return (
     <div>
-      <div>Movie Card Library CRUD</div>
+      <header>Movie Card Library CRUD</header>
       <Router>
-        {/* Switch */}
         <Link to="/movies/new">ADICIONAR CARTÃO</Link>
-        <Route exact path="/" component={MovieList} />
-        <Route exact path="/movies/new" component={NewMovie} />
-        <Route exact path="/movies/:id" component={MovieDetails} />
-        <Route exact path="/movies/:id/edit" component={EditMovie} />
-        <Route exact path="/movies/404-error" component={NotFound} />
-        {/* Switch */}
+        <Switch>
+          <Route exact path="/" component={MovieList} />
+          <Route exact path="/movies/new" component={NewMovie} />
+          <Route exact path="/movies/:id" component={MovieDetails} />
+          <Route exact path="/movies/:id/edit" component={EditMovie} />
+          <Route path="/" component={NotFound} />
+        </Switch>
       </Router>
     </div>
   );
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -24,7 +24,7 @@ class MovieList extends Component {
     const { movies, isLoading } = this.state;
     // Render Loading here if the request is still happening
     return (
-      <div data-testid="movie-list">
+      <div data-testid="movie-list" className="mainContain">
         {isLoading && <Loading />}
         {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
       </div>
