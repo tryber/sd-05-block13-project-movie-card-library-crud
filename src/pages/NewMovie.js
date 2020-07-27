6b70f6ec8eabbcaab2c4585f7ee67a80b881c63f
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-07-27 13:26:16 -0300

    Acho que encontrei o erro.

--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -2,18 +2,26 @@ import React, { Component } from 'react';
 
 import MovieForm from '../components/MovieForm';
 import * as movieAPI from '../services/movieAPI';
+import { Redirect } from 'react-router-dom';
 
 class NewMovie extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      shouldRedirect: false,
+    }
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleSubmit(newMovie) {
+    movieAPI.createMovie(newMovie).then(this.setState({ shouldRedirect: true }))
   }
 
   render() {
-    return (
+    const { shouldRedirect } = this.state;
+    return shouldRedirect? (
+      <Redirect to="/" />
+    ) : (
       <div data-testid="new-movie">
         <MovieForm onSubmit={this.handleSubmit} />
       </div>
