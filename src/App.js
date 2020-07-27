0595711e3ebfeed3e43964f4ce9161cf3a75e78f
Lucas Staroscky <LucasStaroscky@MacBook-Pro-de-Lucas.local>
2020-07-27 11:19:10 -0300

    problem with state and switch order on routes - fixed

--- src/App.js
+++ src/App.js
@@ -9,10 +9,8 @@ function App() {
       <h1>Movie Cards Library CRUD</h1>
       <Switch>
         <Route exact path="/" component={MovieList} />
-        <Route exact path="/" component={MovieDetails} />
-        <Route exact path="/movies/:id" component={MovieDetails} />
-        <Route exact path="/movies/new" component={NewMovie} />
-        <Route exact path="/movies/:id/edit" component={EditMovie} />
+        <Route path="/movies/:id" component={MovieDetails} />
+        <Route path="/movies/:id/edit" component={EditMovie} />
       </Switch>
     </Router>
   );
--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -25,26 +25,23 @@ class MovieDetails extends Component {
   render() {
     const { loading } = this.state;
     if (loading) return <Loading />;
-    if (this.state.movie !== '') {
-      const { title, storyline, imagePath, genre, rating, subtitle } = this.state.movie;
-      return (
-        <section>
-          <div data-testid="movie-details">
-            <img alt="Movie Cover" src={`../${imagePath}`} />
-            <p>{`Title: ${title}`}</p>
-            <p>{`Subtitle: ${subtitle}`}</p>
-            <p>{`Storyline: ${storyline}`}</p>
-            <p>{`Genre: ${genre}`}</p>
-            <p>{`Rating: ${rating}`}</p>
-          </div>
-          <div>
-            <Link to="/movies/:id/edit">EDITAR</Link>
-            <Link to="/">Voltar</Link>
-          </div>
-        </section>
-      );
-    }
-    return null;
+    const { title, storyline, imagePath, genre, rating, subtitle, id } = this.state.movie;
+    return (
+      <section>
+        <div data-testid="movie-details">
+          <img alt="Movie Cover" src={`../${imagePath}`} />
+          <p>{`Title: ${title}`}</p>
+          <p>{`Subtitle: ${subtitle}`}</p>
+          <p>{`Storyline: ${storyline}`}</p>
+          <p>{`Genre: ${genre}`}</p>
+          <p>{`Rating: ${rating}`}</p>
+        </div>
+        <div>
+          <Link to={`/movies/${id}/edit`}>EDITAR</Link>
+          <Link to="/">Voltar</Link>
+        </div>
+      </section>
+    );
   }
 }
 
