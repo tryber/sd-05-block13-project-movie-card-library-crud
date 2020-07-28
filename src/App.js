9f74f44a4888a52115b7837a50d6f2c5fec37a00
Lucas Staroscky <LucasStaroscky@MacBook-Pro-de-Lucas.local>
2020-07-27 22:54:17 -0300

    requirement wasn being recognized by the tests - codeclimate issues fixed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, BrowserRouter } from 'react-router-dom';
 import './App.css';
 import { MovieList, MovieDetails, EditMovie } from './pages';
 
