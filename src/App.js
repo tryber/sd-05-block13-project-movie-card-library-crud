47fdecf0e33c3404a56cae304ae4080835ab1130
Carla Nakajuni <carlanakajuni@hotmail.com>
2020-07-28 00:57:44 -0300

    Renderizando BrowserRouter, componente Loading

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import './App.css'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.css';
+import MovieList from '../src/pages/MovieList';
 
 function App() {
   return (
     <Router>
       <div>Movie Card Library CRUD</div>
+      <Switch>
+        <Route path="/movies/:id/edit" component="" />
+        <Route path="/movies/new" component="" />
+        <Route path="/movies/:id" component="" />
+        <Route path="/" component={MovieList} />
+      </Switch>
     </Router>
   );
 }
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    const { id, title, subtitle, storyline, imagePath, rating } = this.props.movie;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <h4>{title}</h4>
+        <h3>{subtitle}</h3>
+        <p>{storyline}</p>
+        <img src={imagePath} />
+        <Link to={`/movies/${id}`}>VER DETALHES</Link>
+        <p>{rating}</p>
       </div>
     );
   }
