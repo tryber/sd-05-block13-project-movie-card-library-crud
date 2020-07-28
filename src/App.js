1c86a7465032e1c9ff5c9b85d4a10945f043c76f
Lucas Staroscky <LucasStaroscky@MacBook-Pro-de-Lucas.local>
2020-07-27 22:52:31 -0300

    requirement wasn being recognized by the tests

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, BrowserRouter } from 'react-router-dom';
 import './App.css';
 import { MovieList, MovieDetails, EditMovie } from './pages';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <h1>Movie Cards Library CRUD</h1>
       <Switch>
-        <Route exact path="/" component={MovieList} />
         <Route path="/movies/:id" component={MovieDetails} />
+        <Route path="/" component={MovieList} />
         <Route path="/movies/:id/edit" component={EditMovie} />
       </Switch>
-    </Router>
+    </BrowserRouter>
   );
 }
 
