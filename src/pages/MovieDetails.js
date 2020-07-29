febb4d0517871eeb6f1fc7a82a491c7e8bc6ba37
Marylange <marylangess@gmail.com>
2020-07-29 09:36:50 -0300

    codeclimate

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -43,9 +43,9 @@ class MovieDetails extends Component {
 MovieDetails.propTypes = {
   match: PropTypes.shape({
     params: PropTypes.shape({
-      id: PropTypes.number.isRequired
-    })
+      id: PropTypes.number.isRequired,
+    }),
   }),
-}
+};
 
 export default MovieDetails;
