f87435f18219ee17abe1a7ecdfe1e4683332bef5
labelle <beelsdelonge@gmail.com>
2020-08-01 18:16:49 -0300

    Iniciando o projeto novamente

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
 
 function App() {
   return (
-      <div>Movie Card Library CRUD</div>
+    <Router>
+      <div>Movie Card Library CRUD
+          <Switch>
+            <Route exact path="/" component={MovieList}>Lista de Filmes</Route>
+            <Route path="/movies/:id" component={MovieDetails}>Detalhes do Filme</Route>
+            <Route path="/movies/new" component={NewMovie}>Novo Filme</Route>
+            <Route path="/movies/:id/edit" component={EditMovie}>Editar Filme</Route>
+          </Switch>
+      </div>
+    </Router>
   );
 }
 
