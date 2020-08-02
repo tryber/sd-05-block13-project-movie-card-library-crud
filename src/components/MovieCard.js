a1d798b0c8cf840e612343058893af8f746526c9
Lucas Staroscky <LucasStaroscky@MacBook-Pro-de-Lucas.local>
2020-08-02 16:03:10 -0300

    fixing problem with data-testid from moviecard pages

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -6,7 +6,7 @@ class MovieCard extends React.Component {
   render() {
     const { id, title, storyline, imagePath } = this.props.movie;
     return (
-      <div className="movie-card">
+      <div className="movie-card" data-testid="movie-card">
         <img className="image-cards" src={imagePath} alt={title} width="200px" height="100px" />
         <h4 className="title-card">{title}</h4>
         <p className="storyline-card">{storyline}</p>
