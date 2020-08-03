7b307babf88c7692d8fbd2939c32b611712b5889
Giuliano Peixoto <giulianopeixoto@gmail.com>
2020-08-03 20:51:54 -0300

    MovieDetails

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import EditMovie from './pages/EditMovie';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <div>Movie Card Library CRUD</div>
-      <MovieList />
-    </Router>
+      <Switch>
+        <Route exact path="/" component={MovieList} />
+        <Route exact path="/movies/:id" component={MovieDetails} />
+        <Route path="/movies/:id/edit" component={EditMovie} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
