364cf690e60dadbe7f14e559efb02118452d479f
Alexander Mitchell <alexlm.trybe@gmail.com>
2020-08-04 18:34:31 -0300

    Return of the CC

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types'
 
 import { MovieForm, Loading } from '../components';
 import * as movieAPI from '../services/movieAPI';
@@ -42,7 +43,7 @@ class EditMovie extends Component {
     );
   }
 }
-MovieDetails.propTypes = {
+EditMovie.propTypes = {
   match: PropTypes.shape({
     params: PropTypes.shape({
       id: PropTypes.string,
