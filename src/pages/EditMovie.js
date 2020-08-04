8e720326616e6780f1f711feff5339df14c5b62c
Giuliano Peixoto <giulianopeixoto@gmail.com>
2020-08-03 21:40:38 -0300

    codeclimate

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import { MovieForm } from '../components';
 import { Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import * as movieAPI from '../services/movieAPI';
 import Loading from '../components/Loading';
 
