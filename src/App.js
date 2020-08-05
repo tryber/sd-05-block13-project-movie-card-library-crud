3b4f50b587c06c6de6a67785de5bca9058ff051f
Jorge Lucas <jorge.lucas2264@gmail.com>
2020-08-05 18:08:05 -0300

    App.js component adjusted

--- src/App.js
+++ src/App.js
@@ -9,23 +9,12 @@ import NotFound from './pages/NotFound';
 function App() {
   return (
       <Router>
-        <div>Movie Card Library CRUD</div>
-        <header>
-          <Link to="/movies/new">Adicionar Cartao</Link>
-        </header>
         <Switch>
           <Route exact path="/" component={MovieList} />
-          <Route
-            path="/movies/new"
-            render={(props) => <NewMovie {...props} movie={NewMovie} />}
-          />
-          <Route path="/movies/:id" component={MovieDetails} />
+          <Route path="/movies/new" component={NewMovie} />
           <Route path="/movies/:id/edit" component={EditMovie} />
-          <Route
-            path="/movies/"
-            render={(props) => <MovieDetails {...props} />}
-          />
-          <Route path="*" component={NotFound} />
+          <Route path="/movies/:id" component={MovieDetails} />
+          <Route path="/" component={NotFound} />
         </Switch>
       </Router>
   );
