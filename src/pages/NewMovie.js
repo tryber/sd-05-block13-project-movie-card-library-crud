e533ffc634f985debf39c9c3551440c6d051a35a
Isabela Rosa <isabelarosa954@gmail.com>
2020-08-05 22:57:00 -0300

    testando se nome dado a função interfere o avaliador

--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -7,7 +7,7 @@ class NewMovie extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      redirecionar: false,
+      shouldRedirect: false,
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -15,12 +15,12 @@ class NewMovie extends Component {
 
   handleSubmit(newMovie) {
     movieAPI.createMovie(newMovie)
-      .then(() => this.setState({ redirecionar: true }));
+      .then(() => this.setState({ shouldRedirect: true }));
   }
 
   render() {
-    const { redirecionar } = this.state;
-    if (redirecionar) return <Redirect to="/" />;
+    const { shouldRedirect } = this.state;
+    if (shouldRedirect) return <Redirect to="/" />;
     return (
       <div data-testid="new-movie">
         <MovieForm onSubmit={this.handleSubmit} />
