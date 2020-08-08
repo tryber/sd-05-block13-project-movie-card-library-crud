e36a55c455d5b1d3045e570ff7afc1b7be3bbce8
Giuliano Peixoto <giulianopeixoto@gmail.com>
2020-08-08 11:11:00 -0300

    NewMovie

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, } from 'react-router-dom';
 import './App.css';
 import MovieList from './pages/MovieList';
 import MovieDetails from './pages/MovieDetails';
 import EditMovie from './pages/EditMovie';
+import NewMovie from './pages/NewMovie';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <BrowserRouter>
+    <div>
       <div>Movie Card Library CRUD</div>
-      <Switch>
-        <Route exact path="/" component={MovieList} />
-        <Route exact path="/movies/:id" component={MovieDetails} />
-        <Route path="/movies/:id/edit" component={EditMovie} />
-      </Switch>
-    </BrowserRouter>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={MovieList} />
+            <Route path="/movies/new" component={NewMovie} />
+            <Route path="/movies/:id/edit" component={EditMovie} />
+            <Route exact path="/movies/:id" component={MovieDetails} />        
+            <Route path="/:error" component={NotFound} />
+          </Switch>
+        </BrowserRouter>
+    </div>
   );
 }
 
