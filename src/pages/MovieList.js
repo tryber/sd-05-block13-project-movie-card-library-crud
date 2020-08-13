c760964cd9f63df8ff3a9d50b09a8e2a3c921245
Larissa Palombo <larissampalombo@gmail.com>
2020-08-13 16:27:02 -0300

    movie list inicio

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,19 +1,28 @@
+   /* exemplo do site 
+    https://imasters.com.br/front-end/entendendo-estado-de-componentes-com-react-na-pratica */
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
+import Loading from '.../components/Loading';
 
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {movies: ''};
+  }
   render() {
     const { movies } = this.state;
-
-    // Render Loading here if the request is still happening
-
-    return (
-      <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
-      </div>
-    );
+    if movies !== '' {
+      return (
+        <div data-testid="movie-list">
+          {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
+        </div>
+      );
+    }
+    else {
+      <Loading />
+    }
   }
 }
 
