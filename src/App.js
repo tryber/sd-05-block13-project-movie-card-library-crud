a00485255fc112b69bfb6a461fee4420bd7a0ce4
Cass007 <thecastieel07@gmail.com>
2020-08-22 21:03:26 -0300

    madrugadao do crud 1requisito ok

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Router } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
       <div>Movie Card Library CRUD</div>
+      <Router path="/" />
+      <Router path="/movies/:id" />
+      <Router path="/movies/new" />
+      <Router path="/movies/:id/edit" />
     </BrowserRouter>
   );
 }
