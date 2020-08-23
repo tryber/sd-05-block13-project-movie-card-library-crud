bd17505c59df7cd555b08970953c483af5c2b9b0
Cass007 <thecastieel07@gmail.com>
2020-08-23 00:25:42 -0300

    madrugadao bolado

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import MovieCard from './components/MovieCard';
+import MovieList from './pages/MovieList';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
 function App() {
   return (
     // #1 criando as rotas solicitadas
     <BrowserRouter>
       <div>Movie Card Library CRUD</div>
-      <Route path="/" />
-      <Route path="/movies/:id" />
-      <Route path="/movies/new" />
-      <Route path="/movies/:id/edit" />
+      <Switch>
+        <Route exact path="/" component={MovieList} />
+        <Route path="/movies/new" component={NewMovie} />
+        <Route path="/movies/:id/edit" component={EditMovie} />
+        <Route path="/movies/:id" component={MovieCard} />
+      </Switch>
     </BrowserRouter>
   );
 }
