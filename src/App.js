6db00fd7d3f0de1ff0755b6edeecaa2fce960358
fomes <contato.felipeluna@gmail.com>
2020-08-29 08:05:42 -0300

    iniciando projeto

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import MovieList from './pages/MovieList';
 
 function App() {
   return (
+    <div>
       <div>Movie Card Library CRUD</div>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={MovieList} />
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
