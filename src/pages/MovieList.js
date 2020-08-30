e37afdc3fb7ddca7fd2ffbb2983ec60337ca549d
Larissa Palombo <larissampalombo@gmail.com>
2020-08-30 16:24:53 -0300

    cc

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -19,7 +19,7 @@ class MovieList extends Component {
   componentDidMount() {
     movieAPI.getMovies().then((data) => this.setState({ movies: data, loading: false }));
   }
-  
+ 
   render() {
     const { movies, loading } = this.state;
     if (loading) {
