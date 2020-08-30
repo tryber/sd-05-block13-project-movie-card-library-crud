f24f235b21300b3ed86fa75c49941bed40471681
Larissa Palombo <larissampalombo@gmail.com>
2020-08-30 15:46:55 -0300

    del cartão

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -14,6 +14,7 @@ class MovieList extends Component {
       movies: undefined,
       loading: true,
     };
+    this.deletar = this.deletar.bind(this);
   }
   /*  dispara uma ou mais ações após o
    componente ser inserido no DOM (ideal para requisições)
@@ -25,7 +26,11 @@ class MovieList extends Component {
   componentDidMount() {
     movieAPI.getMovies().then((data) => this.setState({ movies: data, loading: false }));
   }
-
+  
+  deletar() {
+    const { id } = this.state.movies;
+    movieAPI.deleteMovie(id);
+  }
   render() {
     const { movies, loading } = this.state;
     if (loading) {
@@ -35,6 +40,7 @@ class MovieList extends Component {
       <div data-testid="movie-list">
         {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
         <Link to={'/movies/new'}>ADICIONAR CARTÃO</Link>
+        <Link to={'/'} onClick={() => this.deletar()}>APAGAR CARTÃO</Link>
       </div>
     );
   }
